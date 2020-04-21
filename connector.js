a870378b19cdb66859ec39329a9fc1c64f7bde67
nickorossa <nick@nickhavard.com>
2020-04-21 15:07:03 +0000

    Lab 4

--- connector.js
+++ connector.js
@@ -28,21 +28,6 @@ class ServiceNowConnector {
     this.options = options;
   }
 
-  /**
-   * @callback iapCallback
-   * @description A [callback function]{@link
-   *   https://developer.mozilla.org/en-US/docs/Glossary/Callback_function}
-   *   is a function passed into another function as an argument, which is
-   *   then invoked inside the outer function to complete some kind of
-   *   routine or action.
-   *
-   * @param {*} responseData - When no errors are caught, return data as a
-   *   single argument to callback function.
-   * @param {error} [errorMessage] - If an error is caught, return error
-   *   message in optional second argument to callback function.
-   */
-
-
 
 /**
  * @memberof ServiceNowConnector
